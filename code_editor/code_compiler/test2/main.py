77fb83075ab1100a03db216ca281e7b75c746060
veeracoder508 <ananthikam6@gmail.com>
2025-07-05 14:31:22 +0530

    Delete code_editor directory

--- code_editor/code_compiler/test2/main.py
+++
@@ -1,7 +0,0 @@
-import split
-
-def main():
-    code = split.SplitFile()
-    print(code.split_file())
-
-main()
--- code_editor/code_compiler/test6/main.py
+++
@@ -1,4 +0,0 @@
-from main_screen import HomeScreenPlan
-
-screen: HomeScreenPlan = HomeScreenPlan()
-screen.display()
